ea1e22154c91d9ba500f016a3a457d563c04960e
berlianalfd <berliana.elfada.tif421@polban.ac.id>
2023-09-28 15:55:54 +0700

    ..

--- react-js/src/components/matakuliah/DaftarMataKuliah.jsx
+++ react-js/src/components/matakuliah/DaftarMataKuliah.jsx
@@ -15,6 +15,22 @@ export default function DaftarMataKuliah() {
         setMataKuliah(result.data);
     };
 
+    async function handleDelete(id_mata_kuliah) {
+        const confirmDelete = window.confirm('Apakah Anda yakin ingin menghapus data ini?');
+        if (!confirmDelete) {
+          return;
+        }
+        try {
+          // Kirim permintaan DELETE ke server dengan parameter id_mata_kuliah
+          await axios.delete(`http://localhost:8082/MataKuliah/delete?id_mata_kuliah=${id_mata_kuliah}`);
+          // Perbarui tampilan dengan menghapus entitas dari state lokal
+          setData((prevData) => prevData.filter((matakuliah) => matakuliah.id_mata_kuliah !== id_mata_kuliah));
+          alert('Data Mata Kuliah berhasil dihapus');
+        } catch (error) {
+          console.error('Error deleting data:', error);
+        }
+      }
+
   return (
     <div className='container'>
         <div className='py-4'>
@@ -41,7 +57,7 @@ export default function DaftarMataKuliah() {
                                 <td>{pl.perguruan_tinggi}</td>
                                 <td>
                                     <Link className='btn btn-outline-primary mx-2'
-                                        to={`/editpenelitian/${matakuliah.id_mata_kuliah}`}    
+                                        to={`/EditMataKuliah/${matakuliah.id_mata_kuliah}`}    
                                     >Edit</Link>
                                     <button className='btn btn-danger mx-2'>Delete</button>
                                 </td>
