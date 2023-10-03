46fde151df9fccae54b21258d308487cbcd3b42b
moaibad <fathulibad.412@gmail.com>
2023-10-03 14:43:00 +0700

    add sbadmin template

--- react-js/src/components/dashboard/Navbar.jsx
+++ react-js/src/components/dashboard/Navbar.jsx
@@ -5,234 +5,272 @@ import { FiUser } from "react-icons/fi";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand navbar-light navbar-bg">
-      <div className="navbar-collapse collapse">
-        <ul className="navbar-nav navbar-align">
-          <li className="nav-item dropdown">
+    <>
+      <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <button
+          id="sidebarToggleTop"
+          className="btn btn-link d-md-none rounded-circle mr-3"
+        >
+          <i className="fa fa-bars"></i>
+        </button>
+
+        <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control bg-light border-0 small"
+              placeholder="Search for..."
+              aria-label="Search"
+              aria-describedby="basic-addon2"
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary" type="button">
+                <i className="fas fa-search fa-sm"></i>
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item dropdown no-arrow d-sm-none">
             <a
-              className="nav-icon dropdown-toggle"
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="searchDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i className="fas fa-search fa-fw"></i>
+            </a>
+            <div
+              className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+              aria-labelledby="searchDropdown"
+            >
+              <form className="form-inline mr-auto w-100 navbar-search">
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control bg-light border-0 small"
+                    placeholder="Search for..."
+                    aria-label="Search"
+                    aria-describedby="basic-addon2"
+                  />
+                  <div className="input-group-append">
+                    <button className="btn btn-primary" type="button">
+                      <i className="fas fa-search fa-sm"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </li>
+
+          <li className="nav-item dropdown no-arrow mx-1">
+            <a
+              className="nav-link dropdown-toggle"
               href="#"
               id="alertsDropdown"
-              data-bs-toggle="dropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
             >
-              <div className="position-relative">
-                <FiBell />
-                <span className="indicator">4</span>
-              </div>
+              <i className="fas fa-bell fa-fw"></i>
+              <span className="badge badge-danger badge-counter">3+</span>
             </a>
             <div
-              className="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0"
+              className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
               aria-labelledby="alertsDropdown"
             >
-              <div className="dropdown-menu-header">4 New Notifications</div>
-              <div className="list-group">
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <i className="text-danger" data-feather="alert-circle" />
-                    </div>
-                    <div className="col-10">
-                      <div className="text-dark">Update completed</div>
-                      <div className="text-muted small mt-1">
-                        Restart server 12 to complete the update.
-                      </div>
-                      <div className="text-muted small mt-1">30m ago</div>
-                    </div>
+              <h6 className="dropdown-header">Alerts Center</h6>
+              <a className="dropdown-item d-flex align-items-center" href="#">
+                <div className="mr-3">
+                  <div className="icon-circle bg-primary">
+                    <i className="fas fa-file-alt text-white"></i>
                   </div>
-                </a>
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <i className="text-warning" data-feather="bell" />
-                    </div>
-                    <div className="col-10">
-                      <div className="text-dark">Lorem ipsum</div>
-                      <div className="text-muted small mt-1">
-                        Aliquam ex eros, imperdiet vulputate hendrerit et.
-                      </div>
-                      <div className="text-muted small mt-1">2h ago</div>
-                    </div>
+                </div>
+                <div>
+                  <div className="small text-gray-500">December 12, 2019</div>
+                  <span className="font-weight-bold">
+                    A new monthly report is ready to download!
+                  </span>
+                </div>
+              </a>
+              <a className="dropdown-item d-flex align-items-center" href="#">
+                <div className="mr-3">
+                  <div className="icon-circle bg-success">
+                    <i className="fas fa-donate text-white"></i>
                   </div>
-                </a>
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <i className="text-primary" data-feather="home" />
-                    </div>
-                    <div className="col-10">
-                      <div className="text-dark">Login from 192.186.1.8</div>
-                      <div className="text-muted small mt-1">5h ago</div>
-                    </div>
+                </div>
+                <div>
+                  <div className="small text-gray-500">December 7, 2019</div>
+                  $290.29 has been deposited into your account!
+                </div>
+              </a>
+              <a className="dropdown-item d-flex align-items-center" href="#">
+                <div className="mr-3">
+                  <div className="icon-circle bg-warning">
+                    <i className="fas fa-exclamation-triangle text-white"></i>
                   </div>
-                </a>
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <i className="text-success" data-feather="user-plus" />
-                    </div>
-                    <div className="col-10">
-                      <div className="text-dark">New connection</div>
-                      <div className="text-muted small mt-1">
-                        Christina accepted your request.
-                      </div>
-                      <div className="text-muted small mt-1">14h ago</div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="dropdown-menu-footer">
-                <a href="#" className="text-muted">
-                  Show all notifications
-                </a>
-              </div>
+                </div>
+                <div>
+                  <div className="small text-gray-500">December 2, 2019</div>
+                  Spending Alert: We've noticed unusually high spending for your
+                  account.
+                </div>
+              </a>
+              <a className="dropdown-item text-center small text-gray-500" href="#">
+                Show All Alerts
+              </a>
             </div>
           </li>
-          <li className="nav-item dropdown">
+
+          <li className="nav-item dropdown no-arrow mx-1">
             <a
-              className="nav-icon dropdown-toggle"
+              className="nav-link dropdown-toggle"
               href="#"
               id="messagesDropdown"
-              data-bs-toggle="dropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
             >
-              <div className="position-relative">
-                <FiMessageSquare />
-              </div>
+              <i className="fas fa-envelope fa-fw"></i>
+              <span className="badge badge-danger badge-counter">7</span>
             </a>
             <div
-              className="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0"
+              className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
               aria-labelledby="messagesDropdown"
             >
-              <div className="dropdown-menu-header">
-                <div className="position-relative">4 New Messages</div>
-              </div>
-              <div className="list-group">
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <img
-                        src="adminkit/img/avatars/avatar-5.jpg"
-                        className="avatar img-fluid rounded-circle"
-                        alt="Vanessa Tucker"
-                      />
-                    </div>
-                    <div className="col-10 ps-2">
-                      <div className="text-dark">Vanessa Tucker</div>
-                      <div className="text-muted small mt-1">
-                        Nam pretium turpis et arcu. Duis arcu tortor.
-                      </div>
-                      <div className="text-muted small mt-1">15m ago</div>
-                    </div>
+              <h6 className="dropdown-header">Message Center</h6>
+              <a className="dropdown-item d-flex align-items-center" href="#">
+                <div className="dropdown-list-image mr-3">
+                  <img
+                    className="rounded-circle"
+                    src="img/undraw_profile_1.svg"
+                    alt="..."
+                  />
+                  <div className="status-indicator bg-success"></div>
+                </div>
+                <div className="font-weight-bold">
+                  <div className="text-truncate">
+                    Hi there! I am wondering if you can help me with a problem
+                    I've been having.
                   </div>
-                </a>
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <img
-                        src="adminkit/img/avatars/avatar-2.jpg"
-                        className="avatar img-fluid rounded-circle"
-                        alt="William Harris"
-                      />
-                    </div>
-                    <div className="col-10 ps-2">
-                      <div className="text-dark">William Harris</div>
-                      <div className="text-muted small mt-1">
-                        Curabitur ligula sapien euismod vitae.
-                      </div>
-                      <div className="text-muted small mt-1">2h ago</div>
-                    </div>
+                  <div className="small text-gray-500">Emily Fowler · 58m</div>
+                </div>
+              </a>
+              <a className="dropdown-item d-flex align-items-center" href="#">
+                <div className="dropdown-list-image mr-3">
+                  <img
+                    className="rounded-circle"
+                    src="img/undraw_profile_2.svg"
+                    alt="..."
+                  />
+                  <div className="status-indicator"></div>
+                </div>
+                <div>
+                  <div className="text-truncate">
+                    I have the photos that you ordered last month, how would you
+                    like them sent to you?
                   </div>
-                </a>
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <img
-                        src="adminkit/img/avatars/avatar-4.jpg"
-                        className="avatar img-fluid rounded-circle"
-                        alt="Christina Mason"
-                      />
-                    </div>
-                    <div className="col-10 ps-2">
-                      <div className="text-dark">Christina Mason</div>
-                      <div className="text-muted small mt-1">
-                        Pellentesque auctor neque nec urna.
-                      </div>
-                      <div className="text-muted small mt-1">4h ago</div>
-                    </div>
+                  <div className="small text-gray-500">Jae Chun · 1d</div>
+                </div>
+              </a>
+              <a className="dropdown-item d-flex align-items-center" href="#">
+                <div className="dropdown-list-image mr-3">
+                  <img
+                    className="rounded-circle"
+                    src="img/undraw_profile_3.svg"
+                    alt="..."
+                  />
+                  <div className="status-indicator bg-warning"></div>
+                </div>
+                <div>
+                  <div className="text-truncate">
+                    Last month's report looks great, I am very happy with the
+                    progress so far, keep up the good work!
                   </div>
-                </a>
-                <a href="#" className="list-group-item">
-                  <div className="row g-0 align-items-center">
-                    <div className="col-2">
-                      <img
-                        src="adminkit/img/avatars/avatar-3.jpg"
-                        className="avatar img-fluid rounded-circle"
-                        alt="Sharon Lessman"
-                      />
-                    </div>
-                    <div className="col-10 ps-2">
-                      <div className="text-dark">Sharon Lessman</div>
-                      <div className="text-muted small mt-1">
-                        Aenean tellus metus, bibendum sed, posuere ac, mattis
-                        non.
-                      </div>
-                      <div className="text-muted small mt-1">5h ago</div>
-                    </div>
+                  <div className="small text-gray-500">Morgan Alvarez · 2d</div>
+                </div>
+              </a>
+              <a className="dropdown-item d-flex align-items-center" href="#">
+                <div className="dropdown-list-image mr-3">
+                  <img
+                    className="rounded-circle"
+                    src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
+                    alt="..."
+                  />
+                  <div className="status-indicator bg-success"></div>
+                </div>
+                <div>
+                  <div className="text-truncate">
+                    Am I a good boy? The reason I ask is because someone told me
+                    that people say this to all dogs, even if they aren't
+                    good...
                   </div>
-                </a>
-              </div>
-              <div className="dropdown-menu-footer">
-                <a href="#" className="text-muted">
-                  Show all messages
-                </a>
-              </div>
+                  <div className="small text-gray-500">Chicken the Dog · 2w</div>
+                </div>
+              </a>
+              <a className="dropdown-item text-center small text-gray-500" href="#">
+                Read More Messages
+              </a>
             </div>
           </li>
-          <li className="nav-item dropdown">
+
+          <div className="topbar-divider d-none d-sm-block"></div>
+
+          <li className="nav-item dropdown no-arrow">
             <a
-              className="nav-icon dropdown-toggle d-inline-block d-sm-none"
+              className="nav-link dropdown-toggle"
               href="#"
-              data-bs-toggle="dropdown"
+              id="userDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
             >
-              <i className="align-middle" data-feather="settings" />
-            </a>
-            <a
-              className="nav-link dropdown-toggle d-none d-sm-inline-block"
-              href="#"
-              data-bs-toggle="dropdown"
-            >
+              <span className="mr-2 d-none d-lg-inline text-gray-600 small">
+                Douglas McGee
+              </span>
               <img
-                src="adminkit/img/avatars/avatar.jpg"
-                className="avatar img-fluid rounded me-1"
-                alt="Charles Hall"
-              />{" "}
-              <span className="text-dark">Charles Hall</span>
+                className="img-profile rounded-circle"
+                src="img/undraw_profile.svg"
+              />
             </a>
-            <div className="dropdown-menu dropdown-menu-end">
-              <a className="dropdown-item" href="pages-profile.html">
-                <FiUser /> Profile
+            <div
+              className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+              aria-labelledby="userDropdown"
+            >
+              <a className="dropdown-item" href="#">
+                <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                Profile
               </a>
               <a className="dropdown-item" href="#">
-                <i className="align-middle me-1" data-feather="pie-chart" />{" "}
-                Analytics
-              </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item" href="index.html">
-                <i className="align-middle me-1" data-feather="settings" />{" "}
-                Settings &amp; Privacy
+                <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                Settings
               </a>
               <a className="dropdown-item" href="#">
-                <i className="align-middle me-1" data-feather="help-circle" />{" "}
-                Help Center
+                <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                Activity Log
               </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item" href="#">
-                Log out
+              <div className="dropdown-divider"></div>
+              <a
+                className="dropdown-item"
+                href="#"
+                data-toggle="modal"
+                data-target="#logoutModal"
+              >
+                <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                Logout
               </a>
             </div>
           </li>
         </ul>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
