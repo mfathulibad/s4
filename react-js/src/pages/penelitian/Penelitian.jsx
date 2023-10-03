45211ebc79a2b2ba53588412c6c5c323ba4980dd
cintianingsih <cintia.ningsih07@gmail.com>
2023-10-03 15:31:51 +0700

    update

--- react-js/src/pages/penelitian/Penelitian.jsx
+++ react-js/src/pages/penelitian/Penelitian.jsx
@@ -2,18 +2,24 @@ import ListPenelitian from "../../components/penelitian/ListPenelitian";
 import Footer from "../../components/dashboard/Footer";
 import Navbar from "../../components/dashboard/Navbar";
 import Sidebar from "../../components/dashboard/Sidebar";
+import ListDosen from "../../components/dosen/ListDosen";
 
 function penelitian(){
     return (
-        <div className="wrapper">
-          <Sidebar />
-    
-          <div className="main">
-            <Navbar />
-            <ListPenelitian/>
-            <Footer />
-          </div>
+      <div className="wrapper">
+        <Sidebar />
+
+        <div id="content-wrapper" className="d-flex flex-column">
+
+              <div id="content">
+
+              <div className="container-fluid">
+                <ListPenelitian/>
+              </div>
+                
+              </div>
         </div>
+      </div>
     );
     
 }
