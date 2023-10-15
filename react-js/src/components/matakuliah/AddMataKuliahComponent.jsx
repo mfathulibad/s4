f34bc75499c3765763933974b5f187a1e27df87a
berlianalfd <berliana.elfada.tif421@polban.ac.id>
2023-10-15 21:26:43 +0700

    up

--- react-js/src/components/matakuliah/AddMataKuliahComponent.jsx
+++ react-js/src/components/matakuliah/AddMataKuliahComponent.jsx
@@ -47,70 +47,68 @@ const AddMataKuliahComponent = () => {
 
   return (
     <div className="container">
-      <div className='table-responsive border p-4 shadow'>
         <h2 className="mt-4">Add Mata Kuliah</h2>
         <form onSubmit={handleSubmit}>
           
-          <div className="mb-1">
+          <div className="mb-3">
             <label htmlFor="nama_mata_kuliah" className="form-label">Nama Mata Kuliah</label>
             <input
               type="text"
               id="nama_mata_kuliah"
               name="nama_mata_kuliah"
-              placeholder='Masukkan Nama Mata Kuliah' 
+              placeholder='' 
               value={formData.nama_mata_kuliah}
               onChange={handleChange}
-              className="form-control col-md-8" // Added col-md-8
+              className="form-control" // Added col-md-8
               required
             />
           </div>
-          <div className="mb-1">
+          <div className="mb-3">
             <label htmlFor="semester" className="form-label">Semester</label>
             <input
               type="number"
               id="semester"
               name="semester"
-              placeholder='Masukkan Semester ' 
+              placeholder=' ' 
               value={formData.semester}
               onChange={handleChange}
-              className="form-control col-md-8" // Added col-md-8
+              className="form-control " // Added col-md-8
               required
             />
           </div>
-          <div className="mb-1">
+          <div className="mb-3">
             <label htmlFor="kode_kelas" className="form-label">Kode Kelas</label>
             <input
               type="text"
               id="kode_kelas"
               name="kode_kelas"
-              placeholder='Masukkan Kode Kelas' 
+              placeholder='' 
               value={formData.kode_kelas}
               onChange={handleChange}
-              className="form-control col-md-8" // Added col-md-8
+              className="form-control " // Added col-md-8
               required
             />
           </div>
-          <div className="mb-1">
+          <div className="mb-3">
             <label htmlFor="perguruan_tinggi" className="form-label">Perguruan Tinggi</label>
             <input
               type="text"
               id="perguruan_tinggi"
               name="perguruan_tinggi"
-              placeholder='Masukkan Perguruan Tinggi' 
+              placeholder=''
               value={formData.perguruan_tinggi}
               onChange={handleChange}
-              className="form-control col-md-8" // Added col-md-8
+              className="form-control" // Added col-md-8
               required
             />
           </div>
           <div className="d-flex justify-content-between">
-            <button type="submit" className="btn btn-outline-primary">Submit</button>
-            <Link className='btn btn-danger' to="/matakuliah">Cancel</Link>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            {/* <Link className='btn btn-danger' to="/matakuliah">Cancel</Link> */}
 
           </div>
         </form>
       </div>
-    </div>
   );
 };
 
